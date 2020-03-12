9cb42b2d755b8d4840344b9b89ce50a4ec85827e
yu tanaka <tanakayuu@tanakayuunoMacBook-Air.local>
2020-03-13 08:28:01 +0900

    [finish]rails-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+
+  def index
+    @books=Book.all
+    @book=Book.new
+  end
+
+  def new
+    @book=Book.new
+  end
+
+  def show
+    @book=Book.find(params[:id])
+  end
+
+  def create
+    @books=Book.all
+  	@book=Book.new(book_params)
+    if @book.save
+       flash[:create]="Book was successfully created."
+       redirect_to book_path(@book.id)
+    else render "index"
+    end
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    @book.update(book_params)
+    flash[:update]="Book was successfully updated."
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    flash[:destroy]="Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title ,:body)
+  end
+end
